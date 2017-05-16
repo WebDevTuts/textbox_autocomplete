618a685f0e740a7d40f4246fee16c35b4d7fb69b
Edwin Chen <edwinchen85@gmail.com>
2017-05-16 23:50:40 +0800

    Define variables

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+ * - Define variables
+ * - Focus the input
+ * - Add event listener for the input keyup and keydown
+ * - Define a function for checking if the input value matches any of the country names
+ * - Define a function for displaying autocomplete results
+ * - Define a function for moving the cursor in the results list
+ * - Define a function for toggling the results list
+ */
+
+var targetInput = document.getElementById('country'),
+    results = document.getElementById('autocomplete-results'),
+    countryList = ['Albania', 'Colombia', 'Cuba', 'El Salvador', 'Jordan', 'Kenya', 'Nepal', 'Romania', 'Sri Lanka', 'Wales'],
+    matches = [],
+    resultsCursor = 0;
